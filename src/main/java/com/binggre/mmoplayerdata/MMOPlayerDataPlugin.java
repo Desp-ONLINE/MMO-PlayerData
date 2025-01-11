4e5aa16fd5fe70a4f447b777fc59e9a4988c30ab
nao505 <nao505@naver.com>
2025-01-11 19:18:17 +0900

    Add AdminCommand and centralize config initialization
    Introduced AdminCommand execution during plugin enablement and centralized configuration setup using Config. Updated database name to a static constant for consistency and maintainability. These changes improve extensibility and clarity in the plugin's initialization process.

--- src/main/java/com/binggre/mmoplayerdata/MMOPlayerDataPlugin.java
+++ src/main/java/com/binggre/mmoplayerdata/MMOPlayerDataPlugin.java
@@ -1,12 +1,16 @@
 package com.binggre.mmoplayerdata;
 
 import com.binggre.binggreapi.BinggrePlugin;
+import com.binggre.mmoplayerdata.commands.AdminCommand;
+import com.binggre.mmoplayerdata.config.Config;
 import com.binggre.mmoplayerdata.listener.PlayerListener;
 import com.binggre.mmoplayerdata.repository.PlayerRepository;
 import lombok.Getter;
 
 @Getter
 public final class MMOPlayerDataPlugin extends BinggrePlugin {
+
+    public static final String DATA_BASE_NAME = "MMO-PlayerData";
 
     @Getter
     private static MMOPlayerDataPlugin instance = null;
@@ -16,9 +20,12 @@ public final class MMOPlayerDataPlugin extends BinggrePlugin {
     @Override
     public void onEnable() {
         instance = this;
-        playerRepository = new PlayerRepository(this, "MMO-PlayerData", "Player", null);
+        Config.getInstance().init();
+
+        playerRepository = new PlayerRepository(this, DATA_BASE_NAME, "Player", null);
         playerRepository.init();
 
+        executeCommand(this, new AdminCommand());
         registerEvents(this, new PlayerListener());
     }
 
